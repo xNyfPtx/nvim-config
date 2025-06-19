4cbc60d1fe19069b0ed2487682b12b82b6dabbeb
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-06-19 14:02:20 -0400

    commit

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,5 +1,18 @@
 local map = vim.keymap.set
 
+-- miscellaneous
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+
 -- nvim-telescope/telescope.nvim
 map("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
 map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>")
@@ -16,5 +29,13 @@ map("n", "<leader>pt", "<cmd>Telescope terms<CR>")
 map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
 map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
 
--- miscellaneous
-map("n", "<leader>pv", '<Cmd>Ex<CR>')
+-- stevearc/conform.nvim
+vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+  conform.format({
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  })
+end
+)
+
