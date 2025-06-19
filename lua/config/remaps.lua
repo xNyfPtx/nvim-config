09f237c305c83d931d9665301b9e21b543078e99
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-06-19 13:26:42 -0400

    commit

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,14 +1,20 @@
--- local builtin = require('telescope.builtin')
--- vim.keymap.set('n', '<leader>ff', builtin.find_files)
---vim.keymap.set('n', '<leader>fg', builtin.live_grep)
--- vim.keymap.set('n', '<leader>fb', builtin.buffers)
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags)
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- vim.keymap.set('n', '<leader>t', '<Cmd>NvimTreeToggle<CR>')
 local map = vim.keymap.set
-map('n', '<leader>ff', '<Cmd>Telescope find_files<CR>')
-map('n', '<leader>fg', '<Cmd>Telescope live_grep<CR>')
-map('n', '<leader>fb', '<Cmd>Telescope buffers<CR>')
-map('n', '<leader>fh', '<Cmd>Telescope help_tags<CR>')
+
+-- nvim-telescope/telescope.nvim
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>")
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+map("n", "<leader>ma", "<cmd>Telescope marks<CR>")
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>")
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>")
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>")
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>")
+map("n", "<leader>pt", "<cmd>Telescope terms<CR>")
+
+-- nvim-tree/nvim-tree.lua
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
+
+-- miscellaneous
 map("n", "<leader>pv", '<Cmd>Ex<CR>')
-map('n', '<leader>t', '<Cmd>NvimTreeToggle<CR>')
