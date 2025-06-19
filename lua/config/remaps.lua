2cf039a3174ca4db62db3c2bee5b21668225d813
xNyfPtx <155147963+xNyfPtx@users.noreply.github.com>
2025-06-19 18:03:31 -0400

    commit

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -33,11 +33,51 @@ map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
 
 -- stevearc/conform.nvim
 vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-  conform.format({
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 500,
-  })
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
 end
 )
 
+-- romgrk/barbar.nvim
+
+local opts = { noremap = true, silent = true }
+
+-- romgrk/barbar.nvim | Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+
+-- romgrk/barbar.nvim | Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- romgrk/barbar.nvim | Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
+-- romgrk/barbar.nvim | Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+
+-- romgrk/barbar.nvim | Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+
+-- romgrk/barbar.nvim | Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+map('n', '<C-s-p>', '<Cmd>BufferPickDelete<CR>', opts)
+
+-- romgrk/barbar.nvim | Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
